b4a2271d6cbccd39e6605519e4ee7549dd414816
hutien <98692228+hutien@users.noreply.github.com>
2022-05-20 18:54:40 +0200

    TAILLE MODIF

--- Assets/Scenes/Script/PickableObject.cs
+++ Assets/Scenes/Script/PickableObject.cs
@@ -21,7 +21,7 @@ public class PickableObject : MonoBehaviour
         float dist = Vector3.Distance(gameObject.transform.position, player.position);
 
         // si - ou = 1.9 unités de distance = on peut ramasser
-        if (dist <= 20f)
+        if (dist <= 5f)
         {
             hasPlayer = true;
         }
